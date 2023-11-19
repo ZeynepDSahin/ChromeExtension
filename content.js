409664420e98a9877f27ab53be30780f35e9ef07
ZeynepDSahin <86682886+ZeynepDSahin@users.noreply.github.com>
2023-11-19 12:30:54 -0600

    Add files via upload

--- content.js
+++ content.js
@@ -1,51 +1,64 @@
 // content.js
 console.log('content.js script started');
 
-// Create a new button element
-console.log('Creating new button element');
-let button = document.createElement('button');
-button.textContent = 'Generate Description';
-console.log('Button element created');
+let lastClickedTextbox = null;
 
-// Append the button to the desired location on the page
-console.log('Appending button to the page');
-// let targetElement = document.querySelector('label[for="product_description"]');
-let targetElement = document.querySelector('#default-section > div.qDEMCDizuqCjrxkUNcha > div.top-padding-15.menu-container-grid.product_details > div.kat-row.attributeGroup.attributeGroup-product_description > section > div.kat-row.kat-row-padding-top-bottom');
-targetElement.insertAdjacentElement('afterend', button);
-console.log('Button appended to the page');
+// Add event listener to all textboxes to track the last clicked one
+let inputs = document.querySelectorAll('input, textarea');
+inputs.forEach(input => {
+  input.addEventListener('focus', function() {
+    lastClickedTextbox = this;
+    console.log('Input field focused');
+  });
+});
 
-// Add an event listener to the button
-console.log('Adding event listener to the button');
-button.addEventListener('click', function() {
-  console.log('Button clicked');
-  let productName = document.querySelector('input[name="productName"]').value;
+// lastClickedTextbox=document.querySelector('#product_description');
+
+function generateDescription() {
+  lastClickedTextbox=document.querySelector('#product_description');
+  console.log('Textbox clicked');
+  let productName = document.querySelector('#reconcile-item-title').value;
   console.log('Product name: ', productName);
-  let shortDescription = document.querySelector('textarea[name="shortDescription"]').value;
-  console.log('Short description: ', shortDescription);
+  lastClickedTextbox.value = productName;
+  // let shortDescription = document.querySelector('textarea[name="shortDescription"]').value;
+  // console.log('Short description: ', shortDescription);
 
   // Make a request to your server-side API
-  console.log('Making a request to the server-side API');
-  fetch('https://your-server.com/api/generate-description', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      productName: productName,
-      shortDescription: shortDescription,
-    }),
-  })
-  .then(response => {
-    console.log('Response received from the server');
-    return response.json();
-  })
-  .then(data => {
-    console.log('Data received from the server: ', data);
-    // Insert detailed description into the page
-    console.log('Inserting detailed description into the page');
-    document.querySelector('textarea[name="detailedDescription"]').value = data.description;
-    console.log('Detailed description inserted into the page');
-  });
-  console.log('Event listener added to the button');
+//   console.log('Making a request to the server-side API');
+//   fetch('https://your-server.com/api/generate-description', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       productName: productName,
+//       shortDescription: shortDescription,
+//     }),
+//   })
+//   .then(response => {
+//     console.log('Response received from the server');
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log('Data received from the server: ', data);
+//     // Insert detailed description into the last clicked textbox
+//     console.log('Inserting detailed description into the textbox');
+//     if (lastClickedTextbox) {
+//       lastClickedTextbox.value = data.description;
+//       console.log('Detailed description inserted into the textbox');
+//     } else {
+//       console.log('No textbox was clicked');
+//     }
+//   });
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "generateDescription") {
+    // Your existing code to generate the description and place it in the text box
+    generateDescription();
+  }
 });
+
 console.log('content.js script ended');
+
+
--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 // popup.js
+// chrome.tabs.executeScript(null, {file: "content.js"});
+// });
 document.getElementById('generate').addEventListener('click', function() {
-    chrome.tabs.executeScript(null, {file: "content.js"});
-  });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "generateDescription"});
+    });
+});
